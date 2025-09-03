10130b0640b119ae62cd24b9ffb5cd10239a7f49
Vijaykumar Yadav <126179477+Vijay1234667@users.noreply.github.com>
2025-09-03 12:52:15 +0530

    changes in yml

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CI/CD Practice</h1>
-      <BrokenComponent /> {/* Intentional error: this component does not exist */}
+      <BrokenComponent /> 
     </div>
   );
 }
